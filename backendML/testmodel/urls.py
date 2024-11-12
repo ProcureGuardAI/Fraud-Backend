6f20133660008f9ff57a22c45efa2dd49a34c39b
clencyc <clency2023@gmail.com>
2024-11-12 13:16:45 +0300

    pdf parser

--- backendML/testmodel/urls.py
+++ backendML/testmodel/urls.py
@@ -4,7 +4,7 @@ from .views import handle_pdf_upload
 
 urlpatterns = [
     # path('testmodel/', TestModelEndpoint.as_view(), name='testmodel'),
-    path('api/upload_pdf/', handle_pdf_upload, name='upload_pdf'),
+    path('upload_pdf/', handle_pdf_upload, name='upload_pdf'),
 ]
 
 # check on the market value
